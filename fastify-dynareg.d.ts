120f2c287ea1c8e710b09cec3e72007a9a06c8ac
greguz <gregolettogiacomo@gmail.com>
2020-08-25 13:47:23 +0200

    Declare fastify version

--- fastify-dynareg.d.ts
+++ fastify-dynareg.d.ts
@@ -1,4 +1,4 @@
-import { FastifyPlugin } from 'fastify';
+import { FastifyPlugin } from 'fastify-plugin'
 
 export declare function dynareg (packageName: string, packageRequired?: boolean): FastifyPlugin
 export default dynareg
--- fastify-dynareg.js
+++ fastify-dynareg.js
@@ -1,23 +1,29 @@
 const build = require('fastify-plugin')
 
 function dynareg (packageName, packageRequired = false) {
-  return build(function (fastify, options, callback) {
-    let packageObject
-    try {
-      packageObject = require(packageName)
-    } catch (err) {
-      if (err instanceof Error && err.code === 'MODULE_NOT_FOUND' && !packageRequired) {
-        fastify.log.debug(`Skipping ${packageName} registration`)
-      } else {
-        callback(err)
-        return
+  return build(
+    function plugin (fastify, options, callback) {
+      let packageObject
+      try {
+        packageObject = require(packageName)
+      } catch (err) {
+        if (err instanceof Error && err.code === 'MODULE_NOT_FOUND' && !packageRequired) {
+          fastify.log.debug(`Skipping ${packageName} registration`)
+        } else {
+          callback(err)
+          return
+        }
       }
+      if (packageObject) {
+        fastify.register(packageObject.default || packageObject, options)
+      }
+      callback()
+    },
+    {
+      name: 'fastify-dynareg',
+      fastify: '^3.0.0'
     }
-    if (packageObject) {
-      fastify.register(packageObject.default || packageObject, options)
-    }
-    callback()
-  })
+  )
 }
 
 dynareg.default = dynareg
